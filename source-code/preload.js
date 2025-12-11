b64d898a916b0294293c688ca751f09e7f128912
michal92299 <chudybyczek2137@gmail.com>
2025-12-11 12:09:39 +0100

    Create preload.js

---
+++ source-code/preload.js
@@ -0,0 +1,16 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  send: (channel, data) => {
+    const validChannels = ['terminal.keystroke', 'terminal.resize'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel, func) => {
+    const validChannels = ['terminal.incomingData'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  },
+});
