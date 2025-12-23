3793a32115b0432fd4eeed248c54bcd58b2f1e5f
michal92299 <chudybyczek2137@gmail.com>
2025-12-23 15:15:11 +0100

    Update main.rs

--- source-code/src/main.rs
+++ source-code/src/main.rs
@@ -1,12 +1,8 @@
 use pyo3::prelude::*;
-use std::fs::read_to_string;
-use std::path::Path;
 
 fn main() -> PyResult<()> {
-    let script_path = Path::new("terminal/main.py");
-    let code = read_to_string(script_path).expect("Failed to read main.py");
-
+    let code = include_str!("../terminal/main.py");
     Python::with_gil(|py| {
-        py.run_bound(&code, None, None)
+        py.run_bound(code, None, None)
     })
 }
